8cfeac838f60bccfdd30aad8e469edac996e6ebe
krijn <krijn@cs.ru.nl>
2023-08-14 15:41:46 +0200

    week,month,year

--- isogeny/isogeny.py
+++ isogeny/isogeny.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime, timedelta
 import os
 import requests
 import unicodeit
@@ -6,6 +6,21 @@ import re
 import mdtex2html
 from bs4 import BeautifulSoup
 
+def classify_date(date_str):
+    parsed_date = datetime.strptime(date_str, '%Y-%m-%d')
+    current_date = datetime.now()
+    
+    time_difference = current_date - parsed_date
+    
+    if time_difference <= timedelta(days=7):
+        return 'last_week'
+    elif time_difference <= timedelta(days=30):
+        return 'last_month'
+    elif time_difference <= timedelta(days=365):
+        return 'last_year'
+    else:
+        return 'other'
+    
 def detexify(text):
     def repl(match):
         return str(unicodeit.replace((match.group(1))))
@@ -25,17 +40,28 @@ total = len(search_results)
 
 file_name = f"isogeny/papers.txt"
 
+full_date = []
+
+for index, result in enumerate(search_results, start=1):
+    title = result.find("strong").get_text().strip()
+    authors = result.find("span", class_="fst-italic").get_text().strip()
+    id = result.find("a", class_="paperlink").get_text().strip()
+    dates = result.find("small", class_="ms-auto").get_text().strip()
+    dates = dates[14:24]
+
+    title = detexify(title)
+
+    formatted = [title, authors, id, dates]
+    full_date.append(formatted)
+
+full_date = sorted(full_date, key=lambda x: x[-1], reverse=True)
+
 with open(file_name, "w") as file:
-    for index, result in enumerate(search_results, start=1):
-        title = result.find("strong").get_text().strip()
-        authors = result.find("span", class_="fst-italic").get_text().strip()
-        id = result.find("a", class_="paperlink").get_text().strip()
-
-        title = detexify(title)
-
-        file.write(f"{title.lower()};;;")
-        file.write(f"{authors.lower()};;;")
-        file.write(f"{id}\n")
+    for chunk in full_date:
+        file.write(f"{chunk[0].lower()};;;")
+        file.write(f"{chunk[1].lower()};;;")
+        file.write(f"{chunk[2]};;;")
+        file.write(f"{classify_date(chunk[3])}\n")
 
 file_name = f"isogeny/log.txt"
 with open(file_name, "a") as file:
