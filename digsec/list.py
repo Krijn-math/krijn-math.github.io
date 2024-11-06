2b6b99e9c6c3cfe6d4b757f5b6f2a55365afb5d9
krijn <krijn@cs.ru.nl>
2024-11-06 21:40:40 +0100

    more radboud

--- digsec/list.py
+++ digsec/list.py
@@ -57,24 +57,22 @@ papers = radboudify(papers)
 lastweek = []
 timer = True
 i = 0
-while timer:
+while len(lastweek) < 100:
     parsed_date = datetime.strptime(papers[i]['lastmodified'], '%Y-%m-%d %H:%M:%S')
     current_date = datetime.now()
     time_diff = current_date - parsed_date
 
-    if time_diff <= timedelta(days=7) or (time_diff <= timedelta(days = 100) and papers[i]['radboud'] == "true"):
+    if time_diff <= timedelta(days=7) or papers[i]['radboud'] == "true":
         lastweek.append(papers[i])
     
     i += 1
     
-    if time_diff > timedelta(days = 100):
-        timer = False
+    # if time_diff > timedelta(days = 100):
+    #     timer = False
 
 lastweek = clean_data(lastweek)
 
 shuffle(lastweek)
-
-print(len(lastweek))
 
 with open('website_data.json', 'w') as f:
     json.dump(lastweek, f, indent=4)
