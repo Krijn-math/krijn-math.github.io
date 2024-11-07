2dfed8cd5c100548cee7ec0b8800c97ea4bf7f13
krijn <krijn@cs.ru.nl>
2024-11-07 17:07:58 +0100

    better scraper

--- isogeny/isogeny_using_pdfscraper.py
+++ isogeny/isogeny_using_pdfscraper.py
@@ -4,36 +4,39 @@ import requests
 import unicodeit
 import re
 from bs4 import BeautifulSoup
-import PyPDF2
+from pypdf import PdfReader
 import io
 
 base = 'http://eprint.iacr.org/'
-key = '/Annots'
-uri = '/URI'
-ank = '/A'
 
 def get_pdf(urlid):
     pdfres = requests.get(base + urlid + ".pdf")
     pdf_io_bytes = io.BytesIO(pdfres.content)
-    pdf = PyPDF2.PdfReader(pdf_io_bytes)
-    return pdf
+    pdf = PdfReader(pdf_io_bytes)
+    pdfpages = [pdf.pages[i].extract_text() for i in range(len(pdf.pages))]
 
-def extract_all_urls(pdf):
-    urls = []
+    return pdfpages
 
-    pages = len(pdf.pages)
 
-    for page in range(pages):
-        page_slice = pdf.pages[page]
-        page_obj = page_slice.get_object()
-        if key in page_obj.keys():
-            ann = page_obj[key]
-            for a in ann:
-                u = a.get_object()
-                if uri in u[ank].keys():
-                    urls.append(u[ank][uri])
 
-    return urls
+
+# URL
+URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+
+def extract_urls(text):
+    return re.findall(URL_REGEX, text, re.IGNORECASE)
+
+def extract_all_urls(pdfpages):
+    res = []
+
+    for page in pdfpages:
+        res += extract_urls(page)
+        
+    if res != []:
+        return res
+    else:
+        print("no repo found")
+        return ''
 
 def extract_gits(urls):
     if urls == []:
@@ -59,7 +62,6 @@ def get_repo(pdf):
         return ['none']
 
     return gits
-
 
 def classify_date(date_str):
     parsed_date = datetime.strptime(date_str, '%Y-%m-%d')
